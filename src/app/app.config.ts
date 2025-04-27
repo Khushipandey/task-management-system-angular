b0f3ece9a9487c63332eac1ceb7610f5cc279a15
Khushipandey <khushi.pandey0714@gmail.com>
2025-04-28 04:49:21 +0530

    using # for routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withHashLocation } from "@angular/router";
 import { routes } from "./app.routes";
 import { HttpClientModule } from "@angular/common/http";
 import { NgBusyModule } from "ng-busy";
@@ -9,7 +9,7 @@ import { InMemoryDataService } from "./core/in-memory-data.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     importProvidersFrom(
       HttpClientModule,
