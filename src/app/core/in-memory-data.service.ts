d2d4e5f55739657b95251c089414e9b301e36f86
khush__ <59875338+Khushipandey@users.noreply.github.com>
2025-04-27 23:25:34 +0530

    setup mock DB for API calls (#2)

---
+++ src/app/core/in-memory-data.service.ts
@@ -0,0 +1,39 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const tasks = [
+      {
+        id: 1,
+        title: "Task One",
+        description: "First task",
+        status: "Pending",
+        dueDate: "2025-04-30",
+      },
+      {
+        id: 2,
+        title: "Task Two",
+        description: "Second task",
+        status: "In Progress",
+        dueDate: "2025-05-05",
+      },
+      {
+        id: 3,
+        title: "Task Three",
+        description: "Third task",
+        status: "Compleated",
+        dueDate: "2025-05-021",
+      },
+      {
+        id: 4,
+        title: "Task Four",
+        description: "Fourth task",
+        status: "In Progress",
+        dueDate: "2025-04-05",
+      },
+    ];
+    return { tasks };
+  }
+}
