3d66ffbbe6f4b8927dc8d3488a30526641ff7e5f
Khushipandey <khushi.pandey0714@gmail.com>
2025-04-28 01:35:18 +0530

    Add filter by status and due date

--- src/app/components/all-task/all-task.component.ts
+++ src/app/components/all-task/all-task.component.ts
@@ -4,15 +4,22 @@ import { Subscription } from "rxjs";
 import { TaskService } from "../../core/task-list.service";
 import { TaskCardComponent } from "../task-card/task-card.component";
 import { SummaryComponent } from "../summary/summary.component";
+import { FormsModule } from "@angular/forms";
+import { Task } from "../../models/task.model";
 
 @Component({
   selector: "app-all-tasks",
   standalone: true,
-  imports: [CommonModule, TaskCardComponent, SummaryComponent],
+  imports: [CommonModule, FormsModule, TaskCardComponent, SummaryComponent],
   templateUrl: "./all-task.component.html",
 })
 export class AllTasksComponent {
-  protected taskList: any;
+  protected taskList: Task[] = [];
+  protected filteredTaskList: Task[] = [];
+
+  protected filterStatus: string = 'All';
+  protected sortOrder: string = 'asc';
+
   private subscription!: Subscription;
 
   constructor(private taskService: TaskService) {}
@@ -25,6 +32,9 @@ export class AllTasksComponent {
   getTaskList() {
     this.taskService.getTasks().then((tasks) => {
       this.taskList = tasks;
+      this.filterStatus = 'All';
+      this.sortOrder = 'asc';
+      this.onFilterChange();
     });
   }
 
@@ -34,6 +44,21 @@ export class AllTasksComponent {
         this.getTaskList();
       }
     );
+  }
+
+  onFilterChange() {
+    let filtered = this.taskList;
+    if(this.filterStatus != 'All') {
+      filtered = filtered.filter(t => t.status === this.filterStatus);
+    }
+
+    filtered.sort((a, b) => {
+      const dateA = new Date(a.dueDate).getTime();
+      const dateB = new Date(b.dueDate).getTime();
+      return this.sortOrder === 'asc' ? dateA - dateB : dateB - dateA;
+    });
+
+    this.filteredTaskList = filtered;
   }
 
   ngOnDestroy() {
